dac8339aed4a971798b9799ba49294eb31aed0f4
Michael Iodice <miodice3@gmail.com>
2020-10-12 11:40:41 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
 
 
+class Book
+
+    attr_accessor :author, :genre, :page_count
+    attr_reader :title
+
+
+    def initialize(title)
+    @title = title    
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+
+    # def title
+    # @title
+    # end
+
+    # def author=(author)
+    #     @author = author
+    # end
+
+    # def author
+    #     @author
+    # end
+
+    # def genre=(genre)
+    #     @genre = genre
+    # end
+
+    # def genre
+    #     @genre
+    # end
+
+    # def page_count=(page_count)
+    #     @page_count = page_count
+    # end
+
+    # def page_count
+    #     @page_count
+    # end
